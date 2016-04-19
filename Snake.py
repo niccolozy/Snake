b7357021db87d62be4ab9c3841d3ef1783e2a005
niccolozy <zy_graysky@hotmail.com>
2016-04-19 23:37:19 +0200

    add auto-advancing of the snake
    a first playable version

--- Snake.py
+++ Snake.py
@@ -5,11 +5,17 @@ class Snake:
 	def __init__(self,head):
 		self.body = []
 		self.body.append(head)
+		self.direction = 'Up'
 
 	def moveTo(self,position,food=False):
 		self.body.append(position)
+		if self.body[-1][0] == self.body[-2][0]:
+			self.direction = 'Left' if self.body[-1][1] == self.body[-2][1] -1 else 'Right'
+		if self.body[-1][1] == self.body[-2][1]:
+			self.direction = 'Up' if self.body[-1][0] == self.body[-2][0] -1 else 'Down'
 		if not food:
 			self.body.pop(0)
+		print(self.direction)
 
 	def getHead(self):
 		return self.body[-1]
--- display.py
+++ display.py
@@ -36,21 +36,17 @@ class Board:
 			if cell == SNAKE:
 				self.board[i] = EMPTY 
 		for (x,y) in self.snake.getBody():
-			print(x,y)
 			self.board[x*self.length+y] = SNAKE
-			print(self.board)
 
 	def draw(self):
-		self.canvas.delete("all")
+		self.canvas.delete("snake","food")
 		size = self.cellSize
 		for i, cell in enumerate(self.board):
 			coords = [size+size*(i/self.length), size+size*(i%self.length)]
-			if cell == EMPTY:
-				self.canvas.create_rectangle(coords[1], coords[0], coords[1]+size, coords[0]+size, outline="black", fill="grey")
-			elif cell == FOOD:
-				self.canvas.create_rectangle(coords[1], coords[0], coords[1]+size, coords[0]+size, outline="black",fill="green")
+			if cell == FOOD:
+				self.canvas.create_rectangle(coords[1], coords[0], coords[1]+size, coords[0]+size, outline="grey",fill="green", tags="food")
 			elif cell == SNAKE:
-				self.canvas.create_rectangle(coords[1], coords[0], coords[1]+size, coords[0]+size, outline="black",fill="black")
+				self.canvas.create_rectangle(coords[1], coords[0], coords[1]+size, coords[0]+size, outline="black",fill="black",tags="snake")
 		
 		self.canvas.create_line(size, size, size*(self.length+1), size)
 		self.canvas.create_line(size*(self.length+1), size, size*(self.length+1), size*(self.height+1))
@@ -59,19 +55,32 @@ class Board:
 		
 		self.canvas.pack()
 
-	def move(self,event):
-		if event.keysym == 'Up':
-			nextPoint = (self.snake.getHead()[0]-1,self.snake.getHead()[1])
-		elif event.keysym == 'Down':
-			nextPoint = (self.snake.getHead()[0]+1,self.snake.getHead()[1])
-		elif event.keysym == 'Left':
-			nextPoint = (self.snake.getHead()[0],self.snake.getHead()[1]-1)
-		elif event.keysym == 'Right':
-			nextPoint = (self.snake.getHead()[0],self.snake.getHead()[1]+1)
+	def keyMove(self,event):
+		if event.keysym == 'Up' and self.snake.direction != "Down":
+			self.snake.direction = event.keysym
+		elif event.keysym == 'Down' and self.snake.direction != "Up":
+			self.snake.direction = event.keysym
+		elif event.keysym == 'Left' and self.snake.direction != "Right":
+			self.snake.direction = event.keysym
+		elif event.keysym == 'Right' and self.snake.direction != "Left":
+			self.snake.direction = event.keysym
 		else:
-			print(event.keysym)
 			return False
-		if 0 <= nextPoint[0] <= self.height and 0 <= nextPoint[1] <= self.length:
+		
+	def autoMove(self):
+		if self.snake.direction == 'Up':
+			nextPoint = (self.snake.getHead()[0]-1,self.snake.getHead()[1])
+		elif self.snake.direction == 'Down':
+			nextPoint = (self.snake.getHead()[0]+1,self.snake.getHead()[1])
+		elif self.snake.direction == 'Left':
+			nextPoint = (self.snake.getHead()[0],self.snake.getHead()[1]-1)
+		elif self.snake.direction == 'Right':
+			nextPoint = (self.snake.getHead()[0],self.snake.getHead()[1]+1)
+		self.move(nextPoint)
+		self.canvas.after(100,self.autoMove)
+
+	def move(self,nextPoint):
+		if 0 <= nextPoint[0] < self.height and 0 <= nextPoint[1] < self.length:
 			if self.board[nextPoint[0]*self.length + nextPoint[1]] == FOOD:
 				self.snake.moveTo(nextPoint,True)
 				self.putFood()
@@ -81,10 +90,10 @@ class Board:
 		self.putSnake()
 		self.draw()
 
-
 tk = Tkinter.Tk()
-game = Board(5,6,tk)
-tk.bind_all('<Key>', game.move)
+game = Board(20,30,tk)
+tk.bind_all('<Key>', game.keyMove)
+tk.after(100,game.autoMove)
 tk.mainloop()
 
 		
